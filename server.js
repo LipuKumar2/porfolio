6ec7c25a88562e7cad7933959a6088c7113955f7
lipu <lipu.giri10@gmail.com>
2025-07-14 19:44:18 +0530

    port

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/contact', async (req, res) => {
+  const { name, email, message } = req.body;
+
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+const mailOptions = {
+  from: process.env.EMAIL_USER, // Your Gmail (matches SMTP login)
+  to: process.env.EMAIL_USER,   // You receive the email
+  subject: `Message from ${name}`,
+  text: `
+    You have received a new message from your portfolio contact form.
+
+    Name: ${name}
+    Email: ${email}
+
+    Message:
+    ${message}
+  `,
+  replyTo: email  // This lets you reply directly to the user
+};
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
